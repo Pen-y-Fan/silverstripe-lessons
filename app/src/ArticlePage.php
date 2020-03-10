f00057962b100172b028cb0bf5ff014e34adac39
Pen-y-Fan <{ID}+{username}@users.noreply.github.com>
2020-03-10 19:53:21 +0000

    Completed lesson 11

--- app/src/ArticlePage.php
+++ app/src/ArticlePage.php
@@ -32,6 +32,10 @@ class ArticlePage extends Page
         'Categories' => ArticleCategory::class,
     ];
 
+    private static $has_many = [
+        'Comments' => ArticleComment::class,
+    ];
+
     private static $owns = [
         'Photo',
         'Brochure',
